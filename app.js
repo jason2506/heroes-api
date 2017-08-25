1f5876eb5c11d1d821f2c2ee7658015d849f60ad
Chi-En Wu <jason2506.mail@gmail.com>
2017-08-25 23:19:51 +0800

    :wrench: Add router module: `heroes`

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const logger = require('morgan');
 
 const index = require('./routes/index');
+const heroes = require('./routes/heroes');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
+app.use('/heroes', heroes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
