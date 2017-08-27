dcbdb17233208cc1284befd8c1c73707311c6817
Chi-En Wu <jason2506.mail@gmail.com>
2017-08-27 18:06:45 +0800

    :hammer: Prepare data and stub before running tests of `router/heroes`

--- test/heroes.spec.js
+++ test/heroes.spec.js
@@ -9,41 +9,79 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('GET /heroes', () => {
-  let requestMock, router, app;
+  // prepare data for test cases
+  const heroes = [
+    {
+      id: 1,
+      name: 'hero1',
+      image: 'hero1.jpg',
+    },
+    {
+      id: 2,
+      name: 'hero2',
+      image: 'hero2.jpg',
+    },
+  ];
+
+  const heroProfiles = [
+    {
+      str: 2,
+      int: 7,
+      agi: 9,
+      luk: 7,
+    },
+    {
+      str: 9,
+      int: 1,
+      agi: 4,
+      luk: 5,
+    },
+  ];
+
+  const heroesWithProfiles = [];
+  for (let i = 0, n = heroes.length; i < n; i++) {
+    const hero = heroes[i];
+    const profile = heroProfiles[i];
+    heroesWithProfiles.push(Object.assign({}, hero, { profile }));
+  }
+
+  const auth = { name: 'name', password: 'password' };
+
+  // replace the `request()` function with mock object
+  const requestStub = sinon.stub();
+  const router = proxyquire('../routes/heroes', { '../utils/request': requestStub });
+  const app = proxyquire('../app', { './routes/heroes': router });
 
   beforeEach(() => {
-    requestMock = sinon.mock('request');
-    requestMock
-      .once()
-      .withExactArgs({ path: '/heroes' });
+    requestStub.reset();
 
-    // replace the `request()` function with mock object
-    router = proxyquire('../routes/heroes', { '../utils/request': requestMock });
-    app = proxyquire('../app', { './routes/heroes': router });
+    // define default stub behavior
+    requestStub
+      .withArgs({ path: '/heroes' })
+      .resolves(JSON.stringify(heroes));
+
+    const postBody = JSON.stringify(auth);
+    requestStub
+      .withArgs({
+        protocol: 'https:',
+        method: 'POST',
+        path: '/auth',
+      }, postBody)
+      .resolves('OK');
+
+    for (let i = 0, n = heroes.length; i < n; i++) {
+      const hero = heroes[i];
+      const profile = heroProfiles[i];
+      requestStub
+        .withArgs({ path: `/heroes/${ hero.id }/profile` })
+        .resolves(JSON.stringify(profile));
+    }
   });
 
   it('should return a list of public heroes', (done) => {
-    const heroes = [
-      {
-        id: 1,
-        name: 'hero1',
-        image: 'hero1.jpg',
-      },
-      {
-        id: 2,
-        name: 'hero2',
-        image: 'hero2.jpg',
-      },
-    ];
-
-    const data = JSON.stringify(heroes);
-    requestMock.resolves(data);
-
     chai.request(app)
       .get('/heroes')
       .end((err, res) => {
-        requestMock.verify();
-
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         expect(res).to.be.json;
@@ -54,15 +92,16 @@ describe('GET /heroes', () => {
   });
 
   it('should return error message if request is rejected', (done) => {
+    // overwrite default `request({ path: '/heroes' })` behavior
     const error = new Error('Unknown Error');
     error.status = 999;
-    requestMock.rejects(error);
+    requestStub
+      .withArgs({ path: '/heroes' })
+      .rejects(error);
 
     chai.request(app)
       .get('/heroes')
       .end((err, res) => {
-        requestMock.verify();
-
         expect(err).to.be.an('Error');
         expect(res).to.have.status(error.status);
         expect(res.body).to.deep.equal(error.message);
